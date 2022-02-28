207acf5b8a5d55d0f6c1866f16eae38663e85d48
pejmanS21 <pezhmansamadi21@gmail.com>
2022-02-28 14:02:35 +0330

    dvc config

---
+++ models/get_weights.sh
@@ -0,0 +1,7 @@
+FILE=face_mask_detection_checkpoint.pth
+if [ ! -f "$FILE" ]; then
+    echo "Downloading $FILE ..."
+    gdown --id 103pKbKfRHcq7EpEZ_6aBRJWL2b_h9zuR
+else
+    echo "$FILE Exist!"
+fi
